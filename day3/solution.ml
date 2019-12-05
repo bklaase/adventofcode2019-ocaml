e823877fb23846672412fb92f0ca58d5ae98bfb9
Bob Klaase <b.klaase@ethon.nl>
2019-12-05 07:00:00 +0100

    day2 wip

--- day3/solution.ml
+++ day3/solution.ml
@@ -1,11 +1,13 @@
+Printexc.record_backtrace true
 (* preprossing *)
-let inputfile = "input.txt"
+let inputfile = "day3/input.txt"
 let input_str_lists = Lib.read_file inputfile (String.split_on_char ',')
 
 type direction = {x: int ; y : int}
 type instruction = {dir: direction ; dist: int}
   
 let get_instruction cmd =
+  print_endline @@ "str: " ^ cmd;
   let d = match (String.get cmd 0) with
     | 'D' -> {x =  0; y = -1}
     | 'U' -> {x =  0; y =  1}
@@ -22,7 +24,19 @@ let input = List.map (fun cmds ->
 
 (* solution *)
 type wires_state = No_wires | Wire_one | Wire_two | Both_wires
+let wires_state_str = function
+  | No_wires -> "no wires" | Wire_one -> "wire one"
+  | Wire_two -> "wire two" | Both_wires -> "both"
 
+let board : ((int * int),wires_state) Hashtbl.t = Hashtbl.create 1000     
+
+let update_board_with_ws board pos ws =
+  let wiring = Hashtbl.find board pos in
+  Hashtbl.replace board pos (match wiring with
+    | No_wires -> ws
+    | Both_wires -> Both_wires
+    | state -> if state == ws then ws else Both_wires)
+  
 let apply_instruction_to_board_pos instr board (x,y) ws =
   let (cx,cy) = (ref x, ref y) in
   let (tx,ty) = (x + instr.dir.x * instr.dist,
@@ -33,20 +47,15 @@ let apply_instruction_to_board_pos instr board (x,y) ws =
     else (
       cx := !cx + instr.dir.x;
       cy := !cy + instr.dir.y;
-      let wiring =  board.(!cx).(!cy) in
-      (board.(!cx).(!cy) <- match wiring with
-                           | No_wires -> ws
-                           | Both_wires -> Both_wires
-                           | state -> if state == ws then ws else Both_wires) ;
+      update_board_with_ws board (!cx,!cy) ws;
       loop ())
   in loop ()
   
-let part1 input =
-  let board = Array.make_matrix 100 100 No_wires in
+let part1 () =
 
 let part2 input  = input
 
 let () =
-  let part1result = (part1 input) in
-  Format.printf "Answers:@.Solution to part one: %d@.Solution to part two: %d@."
-    (part1result.tape.(0)) (part2 input)
+  let part1result = (part1 ()) in
+  Format.printf "Answers:@.Solution to part one: %s@.Solution to part two: ...d@."
+    (wires_state_str part1result.(0).(4)) 
